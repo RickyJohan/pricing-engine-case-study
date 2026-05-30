eb4855dc02a80c1c42ee518d8f2c82400190af2b
ricky <rickyjohan96@gmail.com>
2026-05-30 20:59:04 +0700

    Version V1.01 update

--- pricing_engine.py
+++ pricing_engine.py
@@ -1,1 +1,6 @@
-print("Initial version deployment V1.00")
+import pandas as pd
+product_cost = pd.read_csv("cost_per_material.csv")
+global_supply = pd.read_csv("global_supply.csv")
+
+print(product_cost.head(10))
+print(global_supply.head(10))
